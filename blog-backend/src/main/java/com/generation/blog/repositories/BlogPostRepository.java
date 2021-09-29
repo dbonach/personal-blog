f587d21a5a9dbd8b2b558552377bd0b91f6cf97d
dbonach <deivitlopes@gmail.com>
2021-09-28 23:09:40 -0300

    Add repositories

--- blog-backend/src/main/java/com/generation/blog/repositories/BlogPostRepository.java
+++ blog-backend/src/main/java/com/generation/blog/repositories/BlogPostRepository.java
@@ -1,9 +1,14 @@
 package com.generation.blog.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.generation.blog.entities.BlogPost;
 
+@Repository
 public interface BlogPostRepository extends JpaRepository<BlogPost, Long>{
-
+	
+	public List<BlogPost> findAllByTitleContainingIgnoreCase(String title);
 }
---
+++ blog-backend/src/main/java/com/generation/blog/repositories/BlogPostThemeRepository.java
@@ -0,0 +1,13 @@
+package com.generation.blog.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.generation.blog.entities.BlogPostTheme;
+
+@Repository
+public interface BlogPostThemeRepository extends JpaRepository<BlogPostTheme, Long> {
+	
+	public List<BlogPostTheme> findAllByDescriptionContainingIgnoreCase(String description);
+}
