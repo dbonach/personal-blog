6d2374ffa51b23b695b8f0bc40830d66a5a0e608
dbonach <deivitlopes@gmail.com>
2021-10-14 15:35:52 -0300

    Add endpoint to retrieve all users and update user

--- blog-backend/src/main/java/com/generation/blog/controllers/UserController.java
+++ blog-backend/src/main/java/com/generation/blog/controllers/UserController.java
@@ -1,12 +1,15 @@
 package com.generation.blog.controllers;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +26,11 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@GetMapping("/all")
+	public ResponseEntity <List<User>> getAll() {
+		return ResponseEntity.ok(userService.listarUsuarios());
+	}
+	
 	@PostMapping("/login")
 	public ResponseEntity<UserLogin> autentication(@RequestBody Optional<UserLogin> user) {
 		return userService.login(user).map(resp -> ResponseEntity.ok(resp))
@@ -33,5 +41,13 @@ public class UserController {
 	public ResponseEntity<User> post(@RequestBody User user) {
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(userService.userRegistration(user));
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<User> putUsuario(@RequestBody User user){
+		
+		return userService.updateUser(user)
+			.map(resp -> ResponseEntity.status(HttpStatus.OK).body(resp))
+			.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
 	}
 }
